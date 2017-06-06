0b64cb7429c0193150ec5932af707e9f0af44e66
Vicuko <victorc.fernandez@gmail.com>
2017-06-06 20:03:24 +0200

    Add initial message to inform program start time

--- break_time_reminder.py
+++ break_time_reminder.py
@@ -1,6 +1,7 @@
 import time
 import webbrowser
 
+print("This program started on "+ time.ctime())
 for x in range(3):
     time.sleep(10)
     webbrowser.open("https://www.youtube.com/watch?v=pWn7PYm-W90")
